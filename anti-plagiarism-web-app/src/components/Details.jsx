2ea4498e1f34a9772afe63bc4ca09c8278c6c7f7
Ritvik-lab-coder <ritviksharma822@gmail.com>
2024-02-14 21:50:06 +0530

    Improvements made in data field.

--- anti-plagiarism-web-app/src/components/Details.jsx
+++ anti-plagiarism-web-app/src/components/Details.jsx
@@ -8,58 +8,65 @@ const Details = () => {
                 <div className="text">Enter project details</div>
                 <div className="underline"></div>
             </div>
-            <div className="inputs">
-                <div>
-                    <p>Project title :</p>
-                    <div className="input">
-                        <input type="text" placeholder="Project Title" />
+            <form name="Project Details Database">
+                <div className="inputs">
+                    <div className="input-field">
+                        <p>Project title :</p>
+                        <div className="input">
+                            <input type="text" name="Project Title" placeholder="Project Title" />
+                        </div>
+                    </div>
+                    <div className="input-field">
+                        <p>Team Leader Details :</p>
+                        <div className="input">
+                            <input type="text" name="Leader Name" placeholder="Team Leader Name" />
+                            <input type="text" name="Leader Contact" placeholder="Phone No." />
+                            <input type="email" name="Leader Email" placeholder="Email" />
+                        </div>
+                    </div>
+                    <div className="input-field">
+                        <p>Member 1 Details :</p>
+                        <div className="input">
+                            <input type="text" name="Member 1 Name" placeholder="Member 1 Name" />
+                            <input type="text" name="Member 1 Contact" placeholder="Phone No." />
+                            <input type="email" name="Member 1 Email" placeholder="Email" />
+                        </div>
+                    </div>
+                    <div className="input-field">
+                        <p>Member 2 Details :</p>
+                        <div className="input">
+                            <input type="text" name="Member 2 Name" placeholder="Member 2 Name" />
+                            <input type="text" name="Member 2 Contact" placeholder="Phone No." />
+                            <input type="email" name="Member 2 Email" placeholder="Email" />
+                        </div>
+                    </div>
+                    <div className="input-field">
+                        <p>Member 3 Details (optional) :</p>
+                        <div className="input">
+                            <input type="text" name="Member 3 Name" placeholder="Member 3 Name" />
+                            <input type="text" name="Member 3 Contact" placeholder="Phone No." />
+                            <input type="email" name="Member 3 Email" placeholder="Email" />
+                        </div>
+                    </div>
+                    <div className="input-field">
+                        <p>Mentor Details :</p>
+                        <div className="input">
+                            <input type="text" name="Mentor Name" placeholder="Mentor Name" />
+                            <input type="text" name="Mentor Contact" placeholder="Phone No." />
+                            <input type="email" name="Mentor Email" placeholder="Email" />
+                        </div>
+                    </div>
+                    <div className="input-field">
+                        <p>Abstract of the Project :</p>
+                        <div className="input">
+                            <input type="text" name="Abstract" placeholder="Abstract" />
+                        </div>
                     </div>
                 </div>
-                <div>
-                    <p>Team Leader Details :</p>
-                    <div className="input">
-                        <input type="text" placeholder="Team Leader Name" />
-                        <input type="text" placeholder="Contact" />
-                    </div>
+                <div className="submit-container">
+                    <div className="submit">Sumbit Details</div>
                 </div>
-                <div>
-                    <p>Member 1 Details :</p>
-                    <div className="input">
-                        <input type="text" placeholder="Member 1 Name" />
-                        <input type="text" placeholder="Contact" />
-                    </div>
-                </div>
-                <div>
-                    <p>Member 2 Details :</p>
-                    <div className="input">
-                        <input type="text" placeholder="Member 2 Name" />
-                        <input type="text" placeholder="Contact" />
-                    </div>
-                </div>
-                <div>
-                    <p>Member 3 Details (optional) :</p>
-                    <div className="input">
-                        <input type="text" placeholder="Member 3 Name" />
-                        <input type="text" placeholder="Contact" />
-                    </div>
-                </div>
-                <div>
-                    <p>Mentor Details :</p>
-                    <div className="input">
-                        <input type="text" placeholder="Mentor Name" />
-                        <input type="text" placeholder="Contact" />
-                    </div>
-                </div>
-                <div>
-                    <p>Abstract of the Project :</p>
-                    <div className="input">
-                        <input type="text" placeholder="Abstract" />
-                    </div>
-                </div>
-            </div>
-            <div className="submit-container">
-                <div className="submit">Sumbit Details</div>
-            </div>
+            </form>
         </div>
     )
 }
