a98b72900a8c829936022847d9040136ad6df365
Ritvik-lab-coder <ritviksharma822@gmail.com>
2024-02-16 12:24:53 +0530

    Improved frontend (background & submit).

--- anti-plagiarism-web-app/src/components/Details.jsx
+++ anti-plagiarism-web-app/src/components/Details.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import './Details.css'
 
 const Details = () => {
+    const handleSubmitButton = () => {
+        console.log("onclick fired")
+        let element = document.getElementsByClassName('input')
+        element.inneerHTML = ""
+    }
     return (
         <div className="container">
             <div className="header">
@@ -13,31 +18,31 @@ const Details = () => {
                     <div className="input-field">
                         <p>Project title :</p>
                         <div className="input">
-                            <input type="text" name="Project Title" placeholder="Project Title" />
+                            <input type="text" name="Project Title" placeholder="Project Title" required />
                         </div>
                     </div>
                     <div className="input-field">
                         <p>Team Leader Details :</p>
                         <div className="input">
-                            <input type="text" name="Leader Name" placeholder="Team Leader Name" />
-                            <input type="text" name="Leader Contact" placeholder="Phone No." />
-                            <input type="email" name="Leader Email" placeholder="Email" />
+                            <input type="text" name="Leader Name" placeholder="Team Leader Name" required />
+                            <input type="text" name="Leader Contact" placeholder="Phone No." required />
+                            <input type="email" name="Leader Email" placeholder="Email" required />
                         </div>
                     </div>
                     <div className="input-field">
                         <p>Member 1 Details :</p>
                         <div className="input">
-                            <input type="text" name="Member 1 Name" placeholder="Member 1 Name" />
-                            <input type="text" name="Member 1 Contact" placeholder="Phone No." />
-                            <input type="email" name="Member 1 Email" placeholder="Email" />
+                            <input type="text" name="Member 1 Name" placeholder="Member 1 Name" required />
+                            <input type="text" name="Member 1 Contact" placeholder="Phone No." required />
+                            <input type="email" name="Member 1 Email" placeholder="Email" required />
                         </div>
                     </div>
                     <div className="input-field">
                         <p>Member 2 Details :</p>
                         <div className="input">
-                            <input type="text" name="Member 2 Name" placeholder="Member 2 Name" />
-                            <input type="text" name="Member 2 Contact" placeholder="Phone No." />
-                            <input type="email" name="Member 2 Email" placeholder="Email" />
+                            <input type="text" name="Member 2 Name" placeholder="Member 2 Name" required />
+                            <input type="text" name="Member 2 Contact" placeholder="Phone No." required />
+                            <input type="email" name="Member 2 Email" placeholder="Email" required />
                         </div>
                     </div>
                     <div className="input-field">
@@ -51,20 +56,20 @@ const Details = () => {
                     <div className="input-field">
                         <p>Mentor Details :</p>
                         <div className="input">
-                            <input type="text" name="Mentor Name" placeholder="Mentor Name" />
-                            <input type="text" name="Mentor Contact" placeholder="Phone No." />
-                            <input type="email" name="Mentor Email" placeholder="Email" />
+                            <input type="text" name="Mentor Name" placeholder="Mentor Name" required />
+                            <input type="text" name="Mentor Contact" placeholder="Phone No." required />
+                            <input type="email" name="Mentor Email" placeholder="Email" required />
                         </div>
                     </div>
                     <div className="input-field">
                         <p>Abstract of the Project :</p>
                         <div className="input">
-                            <input type="text" name="Abstract" placeholder="Abstract" />
+                            <input type="text" name="Abstract" placeholder="Abstract" required />
                         </div>
                     </div>
                 </div>
                 <div className="submit-container">
-                    <div className="submit">Sumbit Details</div>
+                    <button className="submit" onClick={handleSubmitButton()}>Sumbit Details</button>
                 </div>
             </form>
         </div>
